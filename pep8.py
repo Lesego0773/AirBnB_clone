46077eb3b4131216dbf54b40fa3840e9924646e6
Lesego0773 <kabelomakokga@gmail.com>
2024-03-12 01:33:44 -0700

    pep8

---
+++ pep8.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#This script passes the pycodestyle checks.
+
+
+def check_fuction():
+    print("Pep8 is super fun as it checks the syle convention")
+
+if __name__ == "__main__":
+    check_fuction()
